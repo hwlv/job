2a49e889a6a32c498c8f4d15af09eb1808514e0b
吕宏旺 <460497764@qq.com>
2018-05-01 23:32:30 +0800

    redux 添加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createStore } from 'redux'
+import logo from './logo.svg'
+import { Button,List } from 'antd-mobile'
+import './App.css'
 
-class App extends Component {
+function counter(state=0,action) {
+    switch (action.type){
+        case 'add':
+            return state+1
+        case 'reduce':
+            return state-1
+        default:
+            return 10
+    }
+}
+// build store
+const store=createStore(counter)
+const init=store.getState()
+console.log(init);
+
+//dispatch action
+store.dispatch({type:'add'})
+const afterAdd=store.getState()
+console.log(afterAdd);
+
+const Item = List.Item;
+const Brief = Item.Brief;
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state={
+        soldiers:['虎子','柱子','王根生']
+      }
+  }
+    state = {
+        disabled: false,
+    }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      hello23
+          {/*<Button type="primary">Button</Button>*/}
+          <List renderHeader={()=>'士兵列表'}>
+              {
+                this.state.soldiers.map(v=>{
+                  return <Item key={v}>{v}</Item>
+                })
+              }
+          </List>
       </div>
     );
   }

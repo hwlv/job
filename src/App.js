45bf2d5cdb8cae72a703fc799c9715a9dc4ad508
lvhongwang <460497764@qq.com>
2018-05-03 10:41:29 +0800

    添加装饰器

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux'
 import logo from './logo.svg'
 import { Button,List } from 'antd-mobile'
 import './App.css'
-
+/**redux */
 function counter(state=0,action) {
     switch (action.type){
         case 'add':
@@ -14,18 +14,26 @@ function counter(state=0,action) {
             return 10
     }
 }
+function listener(){
+    const current=store.getState()
+    console.log(`现在是${current}`);
+}
 // build store
 const store=createStore(counter)
+// 订阅，每次state修改，都会执行listener
+store.subscribe(listener)
+//输出初始值
 const init=store.getState()
 console.log(init);
-
-//dispatch action
+// 提交状态变更的申请
 store.dispatch({type:'add'})
-const afterAdd=store.getState()
-console.log(afterAdd);
+store.dispatch({ type: 'reduce' })
+// const afterAdd=store.getState()
+// console.log(afterAdd);
 
 const Item = List.Item;
 const Brief = Item.Brief;
+/**app */
 class App extends React.Component {
   constructor(props){
       super(props);
@@ -54,5 +62,40 @@ class App extends React.Component {
     );
   }
 }
+function decorateArmour(target, key, descriptor) {
+    const method = descriptor.value;
+    let moreDef = 100;
+    let ret;
+    descriptor.value = (...args)=>{
+        args[0] += moreDef;
+        ret = method.apply(target, args);
+        return ret;
+    }
+    return descriptor;
+}
+
+class Man{
+    constructor(def = 2,atk = 3,hp = 3){
+        this.init(def,atk,hp);
+    }
+
+    @decorateArmour
+    init(def,atk,hp){
+        this.def = def; // 防御值
+        this.atk = atk; // 攻击力
+        this.hp = hp; // 血量
+    }
+    toString(){
+        return `防御力:$,攻击力:$,血量:$`;
+    }
+}
+
+var tony = new Man();
+
+console.log(`当前状态 ===> $`);
+// 输出：当前状态 ===> 防御力:102,攻击力:3,血量:3
+
+
+
 
 export default App;
